48b6c443174ce9b20436d432143b561dbd8046fc
karlaCRM <abraxaskrm@gmail.com>
2023-01-09 23:22:26 -0600

    logre que retorne un solo array con href, etc ....

--- mdLinks.js
+++ mdLinks.js
@@ -1,42 +1,44 @@
+const {log} = console
 const {
-    checkIfPathIsAbsolute,
-    pathconvertToAbs,
-    routeExist,
-    filterTheMdLinks,
-  } = require("./library/paths");
+  checkIfPathIsAbsolute,
+  pathconvertToAbs,
+  routeExist,
+  filterTheMdLinks,
+} = require("./library/paths");
 
-  const { readFiles } = require('./library/links')
+const { readFiles, filterMd } = require('./library/links')
 
- 
-  let pathI = `${process.cwd()}\\test\\prueba`;
-  
-  
+
+let pathI = `${process.cwd()}\\test\\prueba`;
+
+
 
 const mdLinks = (path) => new Promise((resolve,reject) => {
-    const pathAbsolute = checkIfPathIsAbsolute(path)? path : pathconvertToAbs(path)
-      if(routeExist(path)){
-        const filterArray = filterTheMdLinks(pathAbsolute)
-        filterArray.forEach((file) => {
-          readFiles(file)
-          .then(link => {
-            if(link.length >= 1){
-              
-             arr.push(link)
-             console.log(arr)
-            }
-          }
-        )
-          
-        })
-      }
-      else{
-        reject("no existe la ruta")
-      }
-    })
-
-    mdLinks(pathI)
-    .then((response) => {
-      console.log(response);
+  let arrPromises = [];
+  const pathAbsolute = checkIfPathIsAbsolute(path)? path : pathconvertToAbs(path)
+    if(routeExist(path)){
+      const filterArray = filterTheMdLinks(pathAbsolute)
+     if(filterArray.length === 0){
+     reject(log("no hay archivos md"))
+    }
+    //aqui estoy metiendo un promise.All para resolver todas las promesas (viene una por cada for each que se hace al array de links)
+    const promises = []
+    filterArray.forEach(file => promises.push(readFiles(file)) )
     
-    })
-  
+  Promise.allSettled(promises).then(data =>{
+let arr = []
+    data.forEach((el) => arr.push((el.value).flat()))
+   resolve(arr.flat())
+  })
+ 
+    }
+    else{
+      reject(log("no existe la ruta"))
+    }
+    
+  })
+
+  mdLinks(pathI)
+  .then((response) =>{
+    log(response)
+  })
--- test/prueba/text.md
+++ test/prueba/text.md
@@ -1,5 +1,5 @@
 ESTAS DENTRO DEL ARCHIVO 1 !!!
 
-[google](https://www.google.com)
+[google esto es](https://www.google.com)
 [Gatita](https://www.youtube.com/watch?v=msVB4L2Hrfk)
 [ste es el drive](https://drive.google.com/file/d/17i48hwgL0b3gxMNR2f6hRBRnnJZhVf9E/view?usp=share_link)
